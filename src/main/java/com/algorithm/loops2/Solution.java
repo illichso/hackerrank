bd70482c950bafa0fe395778a0e525e2319b1e71
illichso <illichso@gmail.com>
2020-11-08 15:32:12 +0100

    update

---
+++ src/main/java/com/algorithm/loops2/Solution.java
@@ -0,0 +1,18 @@
+package com.algorithm.loops2;
+
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            String result = "";
+
+        }
+        in.close();
+    }
+}
